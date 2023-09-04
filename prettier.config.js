e99be62b32de739e89a127d8f962ba4acfb12780
Anton Sukhodolets <asukhodolets@ptsecurity.com>
2023-09-04 12:26:57 +0700

    feat: :tada: init

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  plugins: ['prettier-plugin-tailwindcss'],
+  printWidth: 120,
+  singleQuote: true,
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  bracketSpacing: true,
+  importOrderCaseInsensitive: true,
+  importOrderMergeDuplicateImports: true,
+  importOrderCombineTypeAndValueImports: true,
+};
